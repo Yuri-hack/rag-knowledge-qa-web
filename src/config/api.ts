fc92e682077d52aad710c2625266f3bbbc72dbf9
Yuanqian Xiang <15690863316@163.com>
2025-10-29 15:27:27 +0800

    ADD:local dev config

---
+++ src/config/api.ts
@@ -0,0 +1,11 @@
+const config = {
+    HOST: process.env.REACT_APP_HOST || '',
+};
+
+// API 端点配置
+export const API_ENDPOINTS = {
+    chat: {
+        // 本地不使用nginx 这里直接拼上localhost:8080
+        stream: `${config.HOST}/api/chat/rag/stream`,
+    }
+};
